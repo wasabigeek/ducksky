4eb77dfbc6d1278e394b8f421f4d07db19c418e6
Nicholas Koh <nkohwl@gmail.com>
2019-08-05 23:03:47 +0800

    Init ducksky

---
+++ lib/ducksky.rb
@@ -0,0 +1,11 @@
+require "ducksky/version"
+
+module Ducksky
+  class Error < StandardError; end
+  # Your code goes here...
+  class Hello
+    def self.say
+      puts 'Hello world!'
+    end
+  end
+end
---
+++ lib/ducksky/version.rb
@@ -0,0 +1,3 @@
+module Ducksky
+  VERSION = "0.1.0"
+end
